9e73f724b9b26f679f9ec504e4f802477b44a918
Arjun Vishanth <arjunvishanth@gmail.com>
2024-03-25 15:01:21 +0530

    Added weather data. Forecast section in progress.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-$(document).ready(function() {
+function populateWeatherData(data) {
+    $('#tempCurrent').html(data.main.temp + '&deg;');
+    let descText = data.weather[0].description;
+    descText = descText.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+        return letter.toUpperCase();
+    });
+    $('#description').text(descText);
+    $('#maxTemp').html('High ' + data.main.temp_max + '&deg; &bull;');
+    $('#minTemp').html('Low ' + data.main.temp_min + '&deg;');
+    $('#tempFeelsLike').html('Feels like ' + data.main.feels_like + '&deg;');
+    $('#windSpeed span').text(data.wind.speed);
+    $('#windDeg span').html(data.wind.deg + '&deg;');
+    $('#humidity span').text(data.main.humidity);
+    $('#pressure span').text(data.main.pressure);
+    $('#visibility span').text(data.visibility);
+}
+
+function populateForecastData(data) {
+    
+}
+
+function getWeatherData() {
     $.ajax({
         url: "http://api.openweathermap.org/data/2.5/weather",
         data: {
@@ -8,33 +29,45 @@ $(document).ready(function() {
         },
         success: function(res) {
             console.log(res);
-
-            // Populate HTML elements with values from API
-            
-            $('#tempCurrent').prepend(res.main.temp); // Temp converted from Kelvin scale to Celsius
-            let descText = res.weather[0].description;
-            descText = descText.toLowerCase().replace(/\b[a-z]/g, function(letter) {
-                return letter.toUpperCase();
-            });
-            $('#description').text(descText);
-            $('#maxTemp').append(' ' + res.main.temp_max + '&deg;');
-            $('#minTemp').append(' ' + res.main.temp_min + '&deg;');
-            $('#tempFeelsLike').append(' ' + res.main.feels_like + '&deg;');
-            $('#windSpeed').text(res.wind.speed);
-            $('#windDeg').text(res.wind.deg);
-            $('#humidity').text(res.main.humidity);
-            $('#pressure').text(res.main.pressure);
-            $('#visibility').text(res.visibility);
-
-            // Periodic refresh TODO
-
-
-
-            // Manual refresh TODO
+            populateWeatherData(res);
         },
         error: function(error) {
             alert("An error occured! Check console");
             console.error(error);
         }
+    });
+}
+
+function getForecastData() {
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/forecast",
+        data: {
+            q: "Hyderabad",
+            units: "metric",
+            appid: "5cac0a072499baa7d097ac0acb3758d7"
+        },
+        success: function(res) {
+            console.log(res);
+        },
+        error: function(error) {
+            alert("An error occured! Check console");
+            console.error(error);
+        }
+    });
+}
+
+// Main
+$(document).ready(function() {
+    getForecastData();
+    getWeatherData();
+
+   /*  // Periodic refresh
+    let intervalId = setInterval(function() {
+        getWeatherData();
+    }, 10000); */
+
+    // Manual refresh
+    $('#refreshButton').click(function() {
+        getWeatherData();
     });
 });
